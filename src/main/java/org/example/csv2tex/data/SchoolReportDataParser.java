08bba60f46cda937c3926f1c5745d417d24155ed
hoobert <bluewhobert@mailbox.org>
2021-09-27 00:01:57 +0200

    comment

--- src/main/java/org/example/csv2tex/data/SchoolReportDataParser.java
+++ src/main/java/org/example/csv2tex/data/SchoolReportDataParser.java
@@ -10,8 +10,15 @@ public class SchoolReportDataParser {
     /**
      * Parses data from a given CSV file into {@link SchoolReportData} structures.
      * <p></p>
-     * It assumes the following columns in the given CSV file, and thus only needs the number of subjects as input parameter.
-     * Other approaches would be just as easily possible.
+     * It assumes the following columns in the given CSV file, and thus only needs the number of subjects as input parameter:
+     * <ul>
+     *     <li>last name</li>
+     *     <li>given name</li>
+     *     <li>date of birth</li>
+     *     <li>a number of column pairs: subject, followed by grade</li>
+     *     <li>remaining columns are assumed to represent boolean values</li>
+     * </ul>
+     * (Other approaches would be just as easily possible)
      *
      * @param numberOfSubjects number of subjects contained in the report data
      */
