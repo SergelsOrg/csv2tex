242f0de867492cf5796c03afaecc5cde07e1ded3
hoobert <bluewhobert@mailbox.org>
2021-09-27 00:36:32 +0200

    update / add JUnit dependencies
    Thanks goes to my IDE for the warning displayed :)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,9 @@ repositories {
 dependencies {
     implementation("org.apache.commons:commons-csv:1.9.0")
     testImplementation("org.assertj:assertj-core:3.21.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
