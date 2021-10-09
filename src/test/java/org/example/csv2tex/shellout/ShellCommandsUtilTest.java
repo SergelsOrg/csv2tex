949ed3ec80ce018d18ed72cab19e32713e3a9af4
hoobert <bluewhobert@mailbox.org>
2021-10-09 10:40:43 +0200

    add utility for command execution and "is installed?" checks

---
+++ src/test/java/org/example/csv2tex/shellout/ShellCommandsUtilTest.java
@@ -0,0 +1,43 @@
+package org.example.csv2tex.shellout;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.csv2tex.shellout.ErrorMessage.PDF_UNITE_NOT_INSTALLED;
+import static org.example.csv2tex.shellout.ErrorMessage.TEX_LIVE_NOT_INSTALLED;
+
+
+class ShellCommandsUtilTest {
+
+    private ShellCommandsUtil sut = new ShellCommandsUtil();
+
+    // TODO run as part of test group that signals that the system has all utilities installed!
+    @Test
+    public void ensureCommandsExist() {
+        assertThat(sut.ensureCommandsExist())
+                .describedAs("should not return any error codes on a system where all utilities are installed")
+                .isEmpty();
+    }
+
+    // TODO run as part of test group that signals that the system has some utilities missing!
+//    @Test
+//    public void ensureCommandsExist_withMissingCommands() {
+//        assertThat(sut.ensureCommandsExist())
+//                .describedAs("should return some error codes on a system where some utilities are missing")
+//                .containsExactly(PDF_UNITE_NOT_INSTALLED, TEX_LIVE_NOT_INSTALLED);
+//    }
+
+    @Test
+    public void commandExitsSuccessfully_withSuccessfulCommand() {
+        assertThat(sut.doesCommandExitSuccessfully("date")).isTrue();
+    }
+    @Test
+    public void commandExitsSuccessfully_withNonexistentCommand() {
+        assertThat(sut.doesCommandExitSuccessfully("nonexistentcommand")).isFalse();
+    }
+    @Test
+    public void commandExitsSuccessfully_withFaultyParameters() {
+        assertThat(sut.doesCommandExitSuccessfully("date", "--unsupportedParameter")).isFalse();
+    }
+
+}
