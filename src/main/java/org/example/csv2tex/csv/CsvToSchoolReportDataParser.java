f9429c06e7dbd7be43a5a7c780ef4924529b782b
hoobert <bluewhobert@mailbox.org>
2021-12-11 16:46:18 +0100

    implement row length validation

--- src/main/java/org/example/csv2tex/csv/CsvToSchoolReportDataParser.java
+++ src/main/java/org/example/csv2tex/csv/CsvToSchoolReportDataParser.java
@@ -70,24 +70,114 @@ public class CsvToSchoolReportDataParser {
     }
 
     private void ensureRowLengths(List<String> headers, List<CSVRecord> recordList) {
-        if(recordList.isEmpty()) {
+        if (recordList.isEmpty()) {
             return;
         }
+
         int headerRowLength = headers.size();
+        Map<Integer, Integer> rowLengths = countRowLengths(recordList);
+
+        boolean lengthChecksPassed = checkForSystematicFaults(headerRowLength, rowLengths);
+        if (lengthChecksPassed) {
+            return;
+        }
+        checkFaultsWithHeuristics(headerRowLength, rowLengths, recordList);
+    }
+
+    // checks cases where all content rows differ from header row
+
+    private Map<Integer, Integer> countRowLengths(List<CSVRecord> recordList) {
         Map<Integer, Integer> rowLengths = new HashMap<>();
         recordList.forEach(record -> {
             int rowLength = record.size();
             Integer existingValue = rowLengths.getOrDefault(rowLength, 0);
             rowLengths.put(rowLength, existingValue + 1);
         });
+        return rowLengths;
+    }
 
-        if (rowLengths.size() == 1 && headerRowLength == rowLengths.keySet().iterator().next()) {
-            // all rows have the same number of columns
-            return;
+    private boolean checkForSystematicFaults(int headerRowLength, Map<Integer, Integer> rowLengths) {
+        if (rowLengths.size() == 1) {
+            Integer contentRowsLength = rowLengths.keySet().iterator().next();
+            if (headerRowLength == contentRowsLength) {
+                // all rows have the same number of columns
+                return true;
+            } else if (headerRowLength > contentRowsLength) {
+                throw new InvalidCsvException(HEADER_LONGER_THAN_ALL_CONTENT);
+            } else if (headerRowLength < contentRowsLength) {
+                throw new InvalidCsvException(HEADER_SHORTER_THAN_ALL_CONTENT);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Heuristic: determine whether to show "some data rows are wrong" or "the header row is wrong"
+     */
+    private void checkFaultsWithHeuristics(float headerRowLength, Map<Integer, Integer> rowLengths, List<CSVRecord> recordList) {
+        float contentRowsShorterThanHeader = 0f;
+        float contentRowsLongerThanHeader = 0f;
+        for (Map.Entry<Integer, Integer> entry : rowLengths.entrySet()) {
+            if (entry.getKey() > headerRowLength) {
+                contentRowsLongerThanHeader += entry.getValue();
+            } else if (entry.getKey() < headerRowLength) {
+                contentRowsShorterThanHeader += entry.getValue();
+            }
         }
 
-        throw new InvalidCsvException(HEADER_SHORTER_THAN_CONTENT);
+        // more than 20% / at least 10 rows off --> assume header error
+        float ratioWrongLengthRows = (contentRowsLongerThanHeader + contentRowsShorterThanHeader) / recordList.size();
+        if (ratioWrongLengthRows > 0.20f || contentRowsShorterThanHeader + contentRowsLongerThanHeader > 9) {
+            if (contentRowsLongerThanHeader > contentRowsShorterThanHeader) {
+                throw new InvalidCsvException(HEADER_SHORTER_THAN_CONTENT, getListOfLongerRows(headerRowLength, recordList));
+            } else {
+                throw new InvalidCsvException(HEADER_LONGER_THAN_CONTENT, getListOfShorterRows(headerRowLength, recordList));
+            }
+        } else { // assume content error
+            if (contentRowsLongerThanHeader > contentRowsShorterThanHeader) {
+                throw new InvalidCsvException(CONTENT_ROW_LONGER_THAN_HEADER, getListOfLongerRows(headerRowLength, recordList));
+            } else {
+                throw new InvalidCsvException(CONTENT_ROW_SHORTER_THAN_HEADER, getListOfShorterRows(headerRowLength, recordList));
+            }
+        }
     }
+
+    private String getListOfLongerRows(float headerRowLength, List<CSVRecord> recordList) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < recordList.size(); i++) {
+            CSVRecord record = recordList.get(i);
+            if (isLongerThanHeader(headerRowLength, record)) {
+                if (sb.length() > 0) {
+                    sb.append(",");
+                }
+                sb.append(i + 1);
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean isLongerThanHeader(float headerRowLength, CSVRecord record) {
+        return record.size() > headerRowLength;
+    }
+
+    private String getListOfShorterRows(float headerRowLength, List<CSVRecord> recordList) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < recordList.size(); i++) {
+            CSVRecord record = recordList.get(i);
+            if (isShorterThanHeader(headerRowLength, record)) {
+                if (sb.length() > 0) {
+                    sb.append(",");
+                }
+                sb.append(i + 1);
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean isShorterThanHeader(float headerRowLength, CSVRecord record) {
+        return record.size() < headerRowLength;
+    }
+
 
     private SchoolReportData createReportDataFromRecord(List<String> headers, CSVRecord rawRowData) {
         SchoolReportData singleStudentData = createBaseData(rawRowData);
