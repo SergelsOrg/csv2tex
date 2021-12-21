0ea2a408f3f06c7407e467dea7f57113aeb6def1
hoobert <bluewhobert@mailbox.org>
2021-12-21 14:12:05 +0100

    implement rendering pipeline

--- src/main/java/org/example/csv2tex/rendering/SchoolReportsRenderer.java
+++ src/main/java/org/example/csv2tex/rendering/SchoolReportsRenderer.java
@@ -1,15 +1,89 @@
 package org.example.csv2tex.rendering;
 
+import org.apache.commons.io.FileUtils;
+import org.example.csv2tex.csv.CsvToSchoolReportDataParser;
+import org.example.csv2tex.data.SchoolReportData;
+import org.example.csv2tex.exception.InvalidCsvException;
+import org.example.csv2tex.exception.RenderingException;
+import org.example.csv2tex.placeholders.NoopPlaceholderReplacer;
+import org.example.csv2tex.placeholders.PlaceholderReplacer;
+import org.example.csv2tex.shellout.ShellCommandsUtil;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.csv2tex.exception.RenderingExceptionCause.NO_DATA;
 
 /**
  * Implements the rendering pipeline, from given CSV and TEX files to output PDF.
  */
 public class SchoolReportsRenderer {
 
-    public Path renderSchoolReportsForGivenFiles(File csvFile, File texFile) {
-        return null;
+    private final CsvToSchoolReportDataParser parser;
+    private final PlaceholderReplacer placeholderReplacer;
+
+
+    public SchoolReportsRenderer() {
+        // FIXME: implement real placeholder replacement
+        placeholderReplacer = new NoopPlaceholderReplacer();
+
+        parser = new CsvToSchoolReportDataParser();
     }
 
+    /**
+     * @param csvFile CSV file containing student report data
+     * @param texFile tex template file with placeholders
+     * @return path to PDF with all school reports
+     * @throws RenderingException  if any exception or error occurs or there is no data, an exception will be thrown
+     * @throws InvalidCsvException if the CSV is considered invalid, an exception will be thrown
+     */
+    public Path renderSchoolReportsForGivenFiles(File csvFile, File texFile) {
+        try {
+            List<SchoolReportData> studentDataList = parser.parseCsvFileToReportDataList(csvFile);
+            if (studentDataList.isEmpty()) {
+                throw new RenderingException(NO_DATA);
+            }
+            String texTemplate = FileUtils.readFileToString(texFile, StandardCharsets.UTF_8);
+            return renderSchoolReportsForGivenFiles(studentDataList, texTemplate);
+        } catch (RenderingException | InvalidCsvException e) {
+            throw e;
+        } catch (Throwable t) {
+            throw new RenderingException(t);
+        }
+    }
+
+    private Path renderSchoolReportsForGivenFiles(List<SchoolReportData> studentDataList, String texTemplate) throws IOException {
+        Path temporaryDirectory = Files.createTempDirectory(getClass().getSimpleName());
+        ShellCommandsUtil shellCommandsInTempDir = new ShellCommandsUtil(temporaryDirectory.toFile());
+        List<String> renderedPdfs = renderStudentReports(studentDataList, texTemplate, temporaryDirectory, shellCommandsInTempDir);
+        Path outputFile = mergePdfs(temporaryDirectory, shellCommandsInTempDir, renderedPdfs);
+        return outputFile;
+    }
+
+    private List<String> renderStudentReports(List<SchoolReportData> studentDataList, String texTemplate, Path temporaryDirectory, ShellCommandsUtil shellCommandsInTempDir) throws IOException {
+        List<String> renderedPdfs = new ArrayList<>();
+        for (int fileNumber = 0; fileNumber < studentDataList.size(); fileNumber++) {
+            renderSingleSchoolReportPdf(studentDataList, texTemplate, temporaryDirectory, shellCommandsInTempDir, renderedPdfs, fileNumber);
+        }
+        return renderedPdfs;
+    }
+
+    private void renderSingleSchoolReportPdf(List<SchoolReportData> studentDataList, String texTemplate, Path temporaryDirectory, ShellCommandsUtil shellCommandsInTempDir, List<String> renderedPdfs, int fileNumber) throws IOException {
+        String texWithReplacedPlaceholders = placeholderReplacer.replacePlaceholdersInTexFile(texTemplate, studentDataList.get(fileNumber));
+        Path temporaryTexFilePath = temporaryDirectory.resolve("schoolReport_" + fileNumber + ".tex").toAbsolutePath();
+        Files.writeString(temporaryTexFilePath, texWithReplacedPlaceholders);
+        shellCommandsInTempDir.runTexi2Pdf(temporaryTexFilePath.toString());
+        renderedPdfs.add("schoolReport_" + fileNumber + ".pdf");
+    }
+
+    private Path mergePdfs(Path temporaryDirectory, ShellCommandsUtil shellCommandsInTempDir, List<String> renderedPdfs) {
+        Path outputFile = temporaryDirectory.resolve("schoolReports.df").toAbsolutePath();
+        shellCommandsInTempDir.runPdfUnite(outputFile.toString(), renderedPdfs);
+        return outputFile;
+    }
 }
