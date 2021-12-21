3355d516ffd1ce8fe8edab729da94053b84b5a36
hoobert <bluewhobert@mailbox.org>
2021-12-21 18:22:53 +0100

    fix test launch, module-wise
    some failing tests because classpath files are not recognized anymore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,14 @@ module org.example.csv2tex {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.apache.commons.io;
+    // without it, CSVFormat.Builder.setHeader will not compile somehow
+    requires java.sql;
 
     // ! Do not commit this commented-in: It causes issues with integration tests for some reason
 //    requires filechooser;
-//    requires commons.csv;
-//    requires org.apache.commons.lang3;
+    // requires commons.csv;
+    requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
 
     opens org.example.csv2tex.csv;
     opens org.example.csv2tex.data;
