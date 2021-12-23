92162e1fb62c4a0ea00e6e3997e6ae23bb992064
hoobert <bluewhobert@mailbox.org>
2021-12-23 15:09:21 +0100

    extract translations to resource files

--- src/main/java/org/example/csv2tex/ui/Csv2TexApplication.java
+++ src/main/java/org/example/csv2tex/ui/Csv2TexApplication.java
@@ -7,12 +7,17 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class Csv2TexApplication extends  Application {
+public class Csv2TexApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Csv2TexApplication.class.getResource("csv2tex-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("translations", Locale.ENGLISH));
+
         VBox loadedParent = fxmlLoader.load();
+
         Scene scene = new Scene(loadedParent, 700, 300);
         stage.setTitle("Pick files for rendering!");
         stage.setScene(scene);
