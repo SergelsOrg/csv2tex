a2c42353d92e3d909da50364cb0633ec2e0e6797
hoobert <bluewhobert@mailbox.org>
2021-12-30 17:43:01 +0100

    update UI on language change
    doing it "manually", any other attempts failed

--- src/main/java/org/example/csv2tex/ui/Csv2TexController.java
+++ src/main/java/org/example/csv2tex/ui/Csv2TexController.java
@@ -11,36 +11,48 @@ import net.raumzeitfalle.fx.filechooser.Skin;
 import net.raumzeitfalle.fx.filechooser.locations.Locations;
 import org.example.csv2tex.exception.RenderingException;
 import org.example.csv2tex.rendering.SchoolReportsRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.ResourceBundle;
 
 public class Csv2TexController {
 
-    private static final Logger logger = LoggerFactory.getLogger(Csv2TexController.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(Csv2TexController.class);
 
     @FXML
     private Node mainLayout;
+
+    @FXML
+    private Button openCsvButton;
+    @FXML
+    private Button openTexButton;
+    @FXML
+    private Button renderPdfButton;
+
     @FXML
     private Label csvFileLabel;
+
     @FXML
     private Label texFileLabel;
+
     @FXML
     public ToggleGroup language;
 
     private File texFile;
     private File csvFile;
     private Locale selectedLanguage = Locale.ENGLISH;
+
+    // called by JAVAFX using reflection
+    public void initialize() {
+        LOGGER.info("Initializing controller");
+    }
 
     @FXML
     public void onOpenTexButtonClick(ActionEvent ignored) {
@@ -173,11 +185,11 @@ public class Csv2TexController {
     }
 
     private void logErrorMessage(RenderingException e) {
-        logger.warn("An exception occurred: " + e.toString(), e);
+        LOGGER.warn("An exception occurred: " + e.toString(), e);
     }
 
     private void logErrorMessage(Exception e) {
-        logger.warn("An exception occurred", e);
+        LOGGER.warn("An exception occurred", e);
     }
 
     private void showErrorMessage(RenderingException e) {
@@ -191,12 +203,12 @@ public class Csv2TexController {
         switch (selectedLanguageButtonId) {
             case "languageSelectEn":
                 selectedLanguage = Locale.ENGLISH;
-                logger.info("Language changed to English");
+                LOGGER.info("Language changed to English");
                 languageChangedTo(selectedLanguage);
                 break;
             case "languageSelectDe":
                 selectedLanguage = Locale.GERMAN;
-                logger.info("Language changed to German");
+                LOGGER.info("Language changed to German");
                 languageChangedTo(selectedLanguage);
                 break;
             default:
@@ -207,6 +219,15 @@ public class Csv2TexController {
     }
 
     private void languageChangedTo(Locale selectedLanguage) {
-        // TODO update UI and whatever is necessary
+
+        ResourceBundle bundle = ResourceBundle.getBundle("translations", selectedLanguage);
+
+        // I tried my best using property bindings instead of this manual update, but did not succeed - it seems this
+        // can be done easily when creating the UI in program, but it's different when using FXML, and none of the
+        // information from the stuff I found worked out, e.g. https://stackoverflow.com/a/19826636/1143126
+        openCsvButton.setText(bundle.getString("openCsvButtonText"));
+        openTexButton.setText(bundle.getString("openTexButtonText"));
+        renderPdfButton.setText(bundle.getString("renderPdfButtonText"));
     }
+
 }
