ab4d561eda2a42f6a6b25884aaba34f2457611d3
hoobert <bluewhobert@mailbox.org>
2021-12-31 23:00:40 +0100

    deduplicate code (DRY principle)

--- src/integrationTest/java/org/example/csv2tex/integrationtest/Csv2TexApplicationNoSoftwareInstalledTest.java
+++ src/integrationTest/java/org/example/csv2tex/integrationtest/Csv2TexApplicationNoSoftwareInstalledTest.java
@@ -17,24 +17,9 @@ import static org.testfx.assertions.api.Assertions.assertThat;
 @ExtendWith(ApplicationExtension.class)
 public class Csv2TexApplicationNoSoftwareInstalledTest {
 
-    // headless testing: no UI will pop up, this should work on CI
     @BeforeAll
     public static void setupForHeadlessTesting() {
-        // https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
-        String isRunningOnCi = System.getProperty("CI");
-        if ("true".equalsIgnoreCase(isRunningOnCi)) {
-            System.setProperty("monocle.platform", "Headless");
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("glass.platform", "Monocle");
-            System.setProperty("embedded", "monocle");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-
-            // System.setProperty("prism.text", "t2k");
-            System.setProperty("prism.text", "native");
-
-            System.setProperty("java.awt.headless", "true");
-        }
+        HeadlessTestsHelper.setupForHeadlessTesting();
     }
 
     @BeforeEach
